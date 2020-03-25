8ad611ac75458df49231f96136cf181eecd0847f
kadrianne <kristine.a.du@gmail.com>
2020-03-25 10:16:11 -0600

    Add movie info.

--- movies.js
+++ movies.js
@@ -16,23 +16,34 @@ function addMovies(movies){
 function createCard(movie, moviesList){
     const movieCard = document.createElement('li')
     
-    movieCard.innerHTML = `<img src='${movie.image}' alt='${movie.title}'>`
-    addToWatchList(movie, movieCard);
-    addToWatchedMovies(movie, movieCard);
+    movieCard.className = 'movie-card'
+    movieCard.innerHTML = `
+    <img src='${movie.image}' alt='${movie.title}'>
+    <section class='movie-info'>
+        <p class='movie-title'>${movie.title}</p>
+        <p class='movie-release'>${movie.release_date}</p>
+        <p class='movie-director'>Director: ${movie.director}</p>
+        <p class='movie-description'>${movie.summary}</p>
+    </section>`
+
+    const movieInfo = movieCard.querySelector('.movie-info')
+
+    addToWatchList(movie, movieInfo);
+    addToWatchedMovies(movie, movieInfo);
     
     moviesList.append(movieCard)
 }
 
 
 // Need to pass in users!!
-function addToWatchList(movie, movieCard){
-    movieCard.innerHTML += `<form action='http://localhost:3000/unwatched_movies?user_id=1&movie_id=${movie.id}' method='POST'>
+function addToWatchList(movie, movieInfo){
+    movieInfo.innerHTML += `<form action='http://localhost:3000/unwatched_movies?user_id=1&movie_id=${movie.id}' method='POST'>
         <input type="submit" value="Add to Watch List">
     </form>`
 }
 
-function addToWatchedMovies(movie, movieCard){
-    movieCard.innerHTML += `<form action='http://localhost:3000/watched_movies?user_id=1&movie_id=${movie.id}' method='POST'>
+function addToWatchedMovies(movie, movieInfo){
+    movieInfo.innerHTML += `<form action='http://localhost:3000/watched_movies?user_id=1&movie_id=${movie.id}' method='POST'>
         <input type="submit" value="I've watched this!">
     </form>`
 }
